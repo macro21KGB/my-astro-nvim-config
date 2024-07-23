3bc68864431a05c2c3188dc9cd3747d89f91e102
Mario De Luca <bloodcreed21@gmail.com>
2024-07-24 00:17:25 +0200

    surroun nvim added

---
+++ lua/plugins/surround.lua
@@ -0,0 +1,10 @@
+
+---@type LazySpec
+return {
+  {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {}
+  }
+}
