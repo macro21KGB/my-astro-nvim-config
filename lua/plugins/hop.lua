de4cc93d4a759a1576b2f507512e463b1e835ea5
Mario De Luca <bloodcreed21@gmail.com>
2025-01-12 14:17:23 +0100

    modified mappings for hopping

--- lua/plugins/hop.lua
+++ lua/plugins/hop.lua
@@ -5,7 +5,7 @@ return {
 		'smoka7/hop.nvim',
 		version = "*",
 		opts = {
-			keys = 'etovxqpdygfblzhckisuran',
+			keys = 'etovxqpdygfblzhckisuran'
 		}
 	}
 }
--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -8,6 +8,16 @@ return {
           -- INFO: Movements
 					["s"] = {"<Cmd>HopWord<Cr>", desc = "hop around the file"},
 					["S"] = {"<Cmd>HopChar1<Cr>", desc = "Hop around with only one char"},
+					["f"] = { function()
+            local directions = require('hop.hint').HintDirection
+  			    require("hop").hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+					end,
+					},
+					["F"] = { function()
+            local directions = require('hop.hint').HintDirection
+  			    require("hop").hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+            end
+					},
           -- INFO: Utils
           ["<Leader><Leader>x"] = {":source %<CR>", desc="Source current file"},
           ["<Leader><Leader>f"] = {"<Cmd>Oil<CR>", desc="File manager using oil.nvim"},
