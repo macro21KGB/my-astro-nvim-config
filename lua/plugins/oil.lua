efc1555ae1f0152c6f970525d87fc442d9fb4d9b
Mario De Luca <bloodcreed21@gmail.com>
2025-01-12 14:36:47 +0100

    oil exit with tab and jk for exit terminal mode

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -5,6 +5,9 @@ return {
     ---@type oil.SetupOpts
     opts = {
       skip_confirm_for_simple_edits = true,
+      keymaps = {
+        ["<Tab>"] = { "actions.parent", mode = "n" },
+      }
     },
     -- Optional dependencies
     -- dependencies = { { "echasnovski/mini.icons", opts = {} } },
