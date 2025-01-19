1c99e1f69e598d38d5d132f020ea6de963fe900a
Mario De Luca <bloodcreed21@gmail.com>
2025-01-19 19:43:22 +0100

    refined controls with mini files

--- lua/polish.lua
+++ lua/polish.lua
@@ -1,5 +1,10 @@
 -- mini.nvim module configuration
 require("mini.ai").setup()
-require("mini.files").setup()
+require("mini.files").setup({
+  mappings = {
+    go_in = "<Tab>",
+    go_out = "<Esc>"
+  }
+})
 require("mini.move").setup()
 require("mini.operators").setup()
