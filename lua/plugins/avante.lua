f476a55f278888c4664f2719601393f17ce97403
Mario De Luca <bloodcreed21@gmail.com>
2025-03-01 00:30:00 +0100

    avante updated and enabled

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -5,7 +5,15 @@ return {
     lazy = false,
     version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
     opts = {
-      -- add any opts here
+      provider = "openrouter",
+      vendors = {
+        openrouter = {
+          __inherited_from = 'openai',
+          endpoint = 'https://openrouter.ai/api/v1',
+          api_key_name = 'OPENROUTER_API_KEY',
+          model = 'google/gemini-2.0-flash-001',
+        },
+      },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
     -- build = "make",
--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,6 +1,6 @@
 return {
   {
     "yetone/avante.nvim",
-    enabled = false
+    enabled = true
   }
 }
