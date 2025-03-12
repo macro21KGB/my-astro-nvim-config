8e8874b196b5a0f8e83b66131a725341bfa140a0
Mario De Luca <bloodcreed21@gmail.com>
2025-03-12 12:05:17 +0100

    default mappings for mini pick

--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -20,7 +20,7 @@ return {
 					},
           -- INFO: Utils
           ["<Leader>fF"] = {":lua MiniFiles.open()<CR>", desc="File Manager using mini.files"},
-          ["<Leader>ff"] = {" :lua MiniPick.builtin.files({tool = 'git'})<CR>", desc="Telescope-like mini pick"},
+          ["<Leader>ff"] = {" :lua MiniPick.builtin.files({tool = 'rg'})<CR>", desc="Telescope-like mini pick"},
           ["<Leader>w"] = {":w<CR>:source<CR>", desc="Save and source the file"},
         }
       }
